4840d1426f253321e1bd279627605bd57257cdfb
Documenter.jl <documenter@juliadocs.github.io>
2022-10-31 12:46:27 +0000

    build based on 5169966

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KM3AcousticsPlots","category":"page"},{"location":"#KM3AcousticsPlots","page":"Home","title":"KM3AcousticsPlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KM3AcousticsPlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KM3AcousticsPlots]","category":"page"}]
+}
